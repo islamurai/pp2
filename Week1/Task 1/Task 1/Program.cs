557d365694b0f8b4454747c0f2717f81eca256f4
islamurai <islam_amangeldy@mail.ru>
2019-02-01 21:10:46 +0300

    success

---
+++ Week1/Task 1/Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+                Console.Write(a[i] + " " + a[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
