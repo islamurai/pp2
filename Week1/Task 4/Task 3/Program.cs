c80c4189ff1da26b711c9568a580683201c78830
islamurai <islam_amangeldy@mail.ru>
2019-02-12 14:49:54 +0300

    hard

---
+++ Week1/Task 4/Task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++) 
+            {
+                for (int j = 0; j <= a; j++)
+                {
+                    if (i>=j)
+
+                    {
+                        Console.Write("[*]");
+
+                    }
+                    if (j==a)
+                    {
+
+
+                        Console.WriteLine();
+
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
