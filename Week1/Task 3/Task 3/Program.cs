66398a2799ec53666c188ba3a839c8f2bcd8c996
islamurai <islam_amangeldy@mail.ru>
2019-02-16 07:19:15 +0300

    changes

--- Week1/Task 3/Task 3/Program.cs
+++ Week1/Task 3/Task 3/Program.cs
@@ -1,81 +1,55 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
 
-
-
-namespace Task2
-
+namespace Task_2
 {
-
-    class Student
-
+    class Student //Создаем класс Student
     {
-
         public string name;
-
         public string id;
-
         public int year;
 
-        public Student()
-
-        {
-
-            name = Console.ReadLine();
-
-            id = Console.ReadLine();
-
-            year = Convert.ToInt32(Console.ReadLine());
-
-        }
-
         public Student(string name, string id, int year)
-
         {
-
-            this.name = name;
-
-            this.id = id;
-
-            this.year = year;
+            this.name = name; // Назначение классу Student имени введенного пользозователем
+            this.id = id; // Назначение классу Student id введенного пользозователем
+            this.year = year; // Назначение классу Student year введенного пользозователем
 
         }
-
-        public void info()
-
+        public void print()
         {
+            Console.WriteLine("Имя студента:  {0}", name); // Вывод имени студента используя параметр
+            Console.WriteLine("ID студента: {0}", id); // Вывод id студента используя параметр
+            if (year == 1)
 
-            Console.WriteLine(name + " " + id + " " + (year + 1));
+
+            {
+                Console.WriteLine("Вы перваш!");
+                year++; // Увелечение года обучения на 1 (инкремент)
+                Console.WriteLine("В cледющим году вы будете второкурсником!");
+            }
+            else if (year == 4)
+            {
+                Console.WriteLine("поздравляю!ты выпускник");
+            }
 
         }
-
     }
 
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
+            Console.WriteLine("Введите свое имя,id и год обучения:");
+            Student st1 = new Student(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine())); // Ввод данных
 
-            Student a = new Student();
+            st1.print();// Вывод данных
 
-            a.info();
+            Console.ReadLine(); // Терминал не закрывается))))
 
-            Student b = new Student("Amangeldi", "18bd", 2018);
-
-            b.info();
-            Console.ReadLine();
         }
-
     }
-
 }
